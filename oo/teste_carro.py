f84692b0302a02180ffaa7c65062588c6c1f35a0
Fabio F. de Souza <fabiofsrj@hotmail.com>
2022-09-11 20:04:22 -0300

    feat:retirei indicação oo do caminho p/ carro.py

--- oo/teste_carro.py
+++ oo/teste_carro.py
@@ -1,10 +1,8 @@
 from unittest import TestCase
-from oo.carro import Motor
+from carro import Motor
 
 class CarroTestCase(TestCase):
     def teste_velocidade_inicial(self):
         motor = Motor()
         motor.velocidade
         self.assertEqual(0, motor.velocidade)
-
-
